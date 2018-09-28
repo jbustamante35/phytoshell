b507525f96cca668e9ed4ddd284c9c6518753576
Sarah Roberts <sarahr@cyverse.org>
2018-09-28 13:51:55 -0700

    removed the backup directory

--- .backup/.180920_backup/langtest/DEwrapper.m
+++
@@ -1,4 +0,0 @@
-function msg = DEwrapper
-%% hello: test function to ensure docker image works
-msg = fprintf("\n\n\nIf you see this, octave is working!!1! :D\nCue obligatory Helow Whorled\n\n\n");
-end
