1a53748f4283c00473b3cce077110335c4bfe290
Sarah Roberts <sarahr@cyverse.org>
2018-10-16 18:41:33 -0700

    modified the docker image to work with the new wrapper script

--- parseConfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Parse through config.json file to extract arguments
-
-set -e
-
-fin=$1
-str=$(echo $fin | tr "\n" " " | cut -f 2 -d [ | cut -f 1 -d ] | tr -d "\"" )
-
-echo $str
--- utils.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-function err_exit() {
-    echo "$@" 1>&2
-    exit 1
-}
